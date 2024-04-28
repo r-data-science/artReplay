0e7c3abd7c4bc987c93d7916e8977353cbab512b
BF <bobby@happycabbage.io>
2024-04-27 21:35:38 -0700

    tagged

--- inst/docker/run.sh
+++ inst/docker/run.sh
@@ -1,5 +1,5 @@
-sudo docker build -t replayapp:latest -f inst/docker/Dockerfile .
-docker tag replayapp:latest bfatemi/replayapp:latest
+sudo docker build -t bfatemi/replayapp:latest -f inst/docker/Dockerfile .
+docker tag bfatemi/replayapp:latest bfatemi/replayapp:latest
 docker push bfatemi/replayapp:latest
 sudo docker run --name replayapp -p 3939:3939 --rm -dt bfatemi/replayapp:latest
 
